de6998d281bdf4e3313c4f4476d64833305299d4
SamuelHGerber <henrylightfoot@gmail.com>
2021-01-13 14:05:29 -0800

    DOM elements in app.js and console logged for validation

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 // import functions and grab DOM elements
+console.log('Hey');
+
+const handButton = document.getElementById('hand-button');
+const yourHand = document.getElementById('your-hand');
+const oppHand = document.getElementById('opp-hand');
+const feedbackMsg = document.getElementById('feedback');
+const handsWon = document.getElementById('hands-won');
+const totalHands = document.getElementById('total-hands');
+
+// const winPercent = document.getElementById('win-percent');
+
+console.log(handButton);
+console.log(yourHand);
+console.log(oppHand);
+console.log(feedbackMsg);
+console.log(handsWon);
+console.log(totalHands);
+
+
 
 // initialize state
+
+
+
 
 // set event listeners to update state and DOM
